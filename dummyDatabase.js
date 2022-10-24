a924d87a471b817bbcc25fe156ec37c1d803ca32
Sai Sompally <ssompally@gmail.com>
2022-10-24 14:14:26 -0400

    dummyDatabase edits

--- dummyDatabase.js
+++ dummyDatabase.js
@@ -1,6 +1,6 @@
 let users = [
-    { _id: 1, name: "John Smith", runs: "18426" },
-    { _id: 2, name: "Fred Ronaldo", runs: "10500" },
-    { _id: 3, name: "Frank Ocean", runs: "10843" }
+    { _id: 1, name: "John Smith", crop_count: "18426" },
+    { _id: 2, name: "Fred Ronaldo", crop_count: "10500" },
+    { _id: 3, name: "Frank Ocean", crop_count: "10843" }
   ];
-  module.exports = players;
+  module.exports = farmers;
