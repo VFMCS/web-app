f5af06442d13ce76ffde7e7985e15170f538753f
AJ Yeung <ajyeung123@gmail.com>
2022-11-02 13:51:51 -0400

    Added farmer cards and modified customer page

---
+++ client/vfm-react-app/src/components/FarmerCard.js
@@ -0,0 +1,37 @@
+import * as React from 'react' 
+import {ThemeProvider, CssBaseline, Typography, Card, CardContent, CardActionArea, CardMedia} from '@mui/material';
+import basketImage from "../images/vegetable-basket.png"
+import theme from "../theme/theme"
+
+// This is a component that displays important information about a product
+const FarmerCard = (props) => {
+    let item = props.item || {name: "Item", location: "0", description: "This is a description"}
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline enableColorScheme />
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image={basketImage}
+                    alt="Product Image"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {item.name}
+                        </Typography>
+                        <Typography gutterBottom variant="subtitle1" component="div">
+                            {item.location}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {item.description}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </ThemeProvider>
+    );
+};
+  
+export default FarmerCard;
--- client/vfm-react-app/src/pages/CustomerLandingPage.js
+++ client/vfm-react-app/src/pages/CustomerLandingPage.js
@@ -1,23 +1,16 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme/theme.js'
 import LandingHeader from '../components/LandingHeader.js';
-//import farmers from '../pages/LandingPage.js'
+import { Divider } from '@mui/material';
+import ProductCard from '../components/ProductCard.js';
+import FarmerCard from '../components/FarmerCard.js';
 
 //Customer landing page upon customer being signed in
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -62,6 +55,10 @@ const CustomerLandingPage = () => {
     */
 
 
+  let farmerArr = Array(10).fill({name: "Steve", location : "1234 Street Street", description : "Coolest farmer in Amherst"})
+  let farmers = [{name : "Greg", location : "152 North Street", descripton : "Great potatoes"}, {name : "John", location : "100 South Street", description : "dope squash"}].concat(farmerArr)
+  let potatoArr = Array(10).fill({name: "Potato", price: "75", description: "This is a potato"})
+  let products = [{name: "Tomato", price: "500", description: "This is a tomato"},{name: "Squash", price: "30", description: "This is a squash"}].concat(potatoArr)
     return (
         <ThemeProvider theme={theme}>
           <CssBaseline />
@@ -149,6 +146,7 @@ const CustomerLandingPage = () => {
           </Box>
           {/* End footer */}
         </ThemeProvider>
+
       );
 };
 
