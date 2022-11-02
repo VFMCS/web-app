1ee345dc6ff6adc1e554e7e3afe2b5d3c2b95780
Sai Sompally <ssompally@gmail.com>
2022-11-02 03:20:11 -0400

    testing iteration

--- client/vfm-react-app/src/pages/CustomerLandingPage.js
+++ client/vfm-react-app/src/pages/CustomerLandingPage.js
@@ -15,37 +15,20 @@ import FarmerCard from '../components/FarmerCard.js';
 //Customer landing page upon customer being signed in
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-//Get featured farmers
-let farmerInfo = {};
-fetch('http://localhost:3001/farmers').then(response => response.json()).then(data => farmerInfo = data.data)
-      .catch(err => console.error(err));
-
-console.log(farmerInfo)
 
 const CustomerLandingPage = () => {
-    const [featuredFarmersInfo, setFeaturedFarmersInfo] = React.useState([
-    {
-      "first_name": "John",
-      "last_name": "Farmer",
-      "about_me": "I can't get enough of farming"
-  },
-  {
-      "first_name": "Steve",
-      "last_name": "Farmer",
-      "about_me": "I'm nice at farming"
-  },
-  {
-      "first_name": "Henry",
-      "last_name": "Notshah",
-      "about_me": "I am a farmer that distributes vegetables in Amherst Center. All of my vegetables are quality grown and I use advanced farming techniques. No GMOs used"
-  }
-]) 
+  const [featuredFarmersInfo, setFeaturedFarmersInfo] = React.useState({}) // capture data from GET request
 
-    console.log(featuredFarmersInfo);
+    React.useEffect(() => {
+          fetch('http://localhost:3001/farmers').then(response => response.json()).then(data => console.log(data))
+          .catch(err => console.error(err));
+    }, [])
 
-    featuredFarmersInfo[0].image = 'https://thumbs.dreamstime.com/b/pengzhou-china-farmer-bicycle-16230465.jpg'
-    featuredFarmersInfo[1].image = 'https://www.gazettenet.com/getattachment/d3ceb3a3-e856-44a5-9557-d76987b2d7bf/hl090916-storyplace-ph06'
-    featuredFarmersInfo[2].image = 'https://usfarmersandranchers.org/wp-content/uploads/2022/10/DougSaathoff-880x596.jpg'
+    //console.log(featuredFarmersInfo);
+
+    //featuredFarmersInfo[0].image = <img src='https://thumbs.dreamstime.com/b/pengzhou-china-farmer-bicycle-16230465.jpg'/>
+    //featuredFarmersInfo[1].image = <img src='https://www.gazettenet.com/getattachment/d3ceb3a3-e856-44a5-9557-d76987b2d7bf/hl090916-storyplace-ph06'/>
+    //featuredFarmersInfo[2].image = <img src='https://usfarmersandranchers.org/wp-content/uploads/2022/10/DougSaathoff-880x596.jpg'/>
 
 
     /*
@@ -53,7 +36,6 @@ const CustomerLandingPage = () => {
           {featuredFarmersInfo.map(farmer => <div>{farmer.name}</div>)}
     </div>
     */
-
 
   let farmerArr = Array(10).fill({name: "Steve", location : "1234 Street Street", description : "Coolest farmer in Amherst"})
   let farmers = [{name : "Greg", location : "152 North Street", descripton : "Great potatoes"}, {name : "John", location : "100 South Street", description : "dope squash"}].concat(farmerArr)
@@ -112,7 +94,7 @@ const CustomerLandingPage = () => {
                           height: 300,
                           pt: '0%',
                         }}
-                        image={farmer.image}
+                        image="https://source.unsplash.com/random"
                         //image =
                         alt="random"
                       />
