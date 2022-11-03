3946d9ac8c86f826145f02f50041bf2b15b6b42e
Frankie Furnelli <fjfur10@comcast.net>
2022-11-03 15:56:10 -0400

    starting headers

--- client/vfm-react-app/src/components/ConsumerHeader.js
+++ client/vfm-react-app/src/components/ConsumerHeader.js
@@ -1,22 +1,16 @@
 import * as React from 'react' 
 import { AppBar, Toolbar, Button, Box, ThemeProvider, CssBaseline, Typography, IconButton} from '@mui/material';
-import headerLogo from "../../images/logo-simple.png"
-import theme from "../../theme/theme"
+import headerLogo from "../images/logo-simple.png"
+import theme from "../theme/theme"
 import MenuIcon from "@mui/icons-material/Menu"
 import { useNavigate } from "react-router-dom";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import SearchBar from '../SearchBar';
-import ShoppingCartButton from '../buttons/ShoppingCartButton';
-import ConsumerSidebar from '../sidebars/ConsumerSidebar';
+import SearchBar from './SearchBar';
+import ShoppingCartButton from './buttons/ShoppingCartButton';
 
 // A Header Component used by the Consumer
 // Contains: Menu button, Logo, Dashboard button, and Products Button
 const ConsumerHeader = () => {
     let navigate = useNavigate()
-    let [sideBarOpen, setSidebarState] = React.useState(false)
-    let toggleSidebar = () => {
-        setSidebarState(!sideBarOpen)
-    }
 
     return (
         <ThemeProvider theme={theme}>
@@ -24,10 +18,9 @@ const ConsumerHeader = () => {
             <Box sx={{flexGrow: 1}}>
                 <AppBar position='static' sx={{background: "white"}}>
                     <Toolbar> 
-                        <IconButton onClick={toggleSidebar}>
+                        <IconButton>
                             <MenuIcon />
                         </IconButton>
-                        <ConsumerSidebar isOpen={sideBarOpen} toggle={toggleSidebar} />
                         <Box sx={{flexGrow: 1}}>
                         <Button>
                             <img src={headerLogo} alt="Logo" />
@@ -36,12 +29,7 @@ const ConsumerHeader = () => {
                             </Typography>
                         </Button>
                         </Box>
-                        <Box sx={{flexGrow: 2}}>
-                            <Button  variant="contained" sx={{ bgcolor: "primary.dark", fontWeight: "bold"}} startIcon={<AddCircleIcon fontSize="large" />}>
-                                Create Product
-                            </Button>
-                        </Box>
-                        <Box sx={{flexGrow: 3}}>
+                        <Box sx={{flexGrow: 4}}>
                         <SearchBar />
                         </Box>
                         <ShoppingCartButton />
