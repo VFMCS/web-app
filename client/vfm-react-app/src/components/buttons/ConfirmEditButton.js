1ca1e3915386821501df24d5509098675e81fd59
nealshuh <nealmshah11@gmail.com>
2022-11-04 17:44:59 -0400

    Default images functionality and files
    The files for the default image were added, the text fields can only contain numbers now

--- client/vfm-react-app/src/components/buttons/ConfirmEditButton.js
+++ client/vfm-react-app/src/components/buttons/ConfirmEditButton.js
@@ -1,5 +1,14 @@
 import {Button} from "@mui/material"
 
+//Button used on the bottom of the Modify Item screens
+//Can be customized as both a 'Save changes' and 'cancel button'
+//Used for larger buttons for primary functionalities on webpages
+//Takes three props:
+//variant: string
+//size: string
+//color: string
+//label: string (will be converted to all caps)
+
 const ConfirmEditButton = (props) => {
     return (
         <Button variant={props.variant || "text"} style={{ height: '100%', width: '50%'}} size={props.size || "medium"} color={props.color || "success"} sx={{fontWeight: "bold"}}>{props.label || "Publish"}</Button>
