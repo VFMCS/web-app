2d81e3ed579dc92ab6c9322f3680d211662a10ab
lucamne <71897009+lucamne@users.noreply.github.com>
2022-11-04 15:56:59 -0400

    Set toFarmerProfile to temporary function to prevent compiler error

--- client/vfm-react-app/src/components/FarmerCard.js
+++ client/vfm-react-app/src/components/FarmerCard.js
@@ -1,5 +1,5 @@
-import * as React from 'react' 
-import {ThemeProvider, CssBaseline, Typography, Card, CardContent, CardActionArea, CardMedia, Button} from '@mui/material';
+import * as React from 'react'
+import { ThemeProvider, CssBaseline, Typography, Card, CardContent, CardActionArea, CardMedia, Button } from '@mui/material';
 import basketImage from "../images/vegetable-basket.png"
 import theme from "../theme/theme"
 import { useNavigate } from 'react-router-dom';
@@ -7,17 +7,18 @@ import { useNavigate } from 'react-router-dom';
 // This is a component that displays important information about a product
 const FarmerCard = (props) => {
     //let item = props.item || {name: "Item", location: "0", description: "This is a description"}
+    const toFarmerProfile = () => 0;
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline enableColorScheme />
             <Card sx={{ maxWidth: 345 }}>
                 <CardActionArea>
                     <CardMedia
-                    component="img"
-                    height="300"
-                    image={props.item.image_url}
-                  />
-                
+                        component="img"
+                        height="300"
+                        image={props.item.image_url}
+                    />
+
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
                             {props.item.first_name}
@@ -29,7 +30,7 @@ const FarmerCard = (props) => {
                             {props.item.about_me}
                         </Typography>
                         <Button onClick={toFarmerProfile}>
-                            <Typography sx={{ color: "primary.dark", fontWeight: "bold"}}>
+                            <Typography sx={{ color: "primary.dark", fontWeight: "bold" }}>
                                 View Profile
                             </Typography>
                         </Button>
@@ -39,5 +40,5 @@ const FarmerCard = (props) => {
         </ThemeProvider>
     );
 };
-  
+
 export default FarmerCard;
