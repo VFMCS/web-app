b804dd166e0da726c8f171873785aaca03179051
lucamne <negrislmn@gmail.com>
2022-11-04 07:19:54 -0400

    User api endpoints added

--- server/user-api/routes.js
+++ server/user-api/routes.js
@@ -3,6 +3,11 @@ const controller = require('./controllers');
 
 const router = Router()
 
-router.get('/',controller.getUsers);
+router.get("/",controller.getUsers);
+router.get("/:user_id",controller.getUserByID);
+router.get("/farmers",controller.getFarmers);
+router.post("/",controller.createUser);
+router.delete("/:user_id",controller.deleteUser);
+
 
 module.exports = router;
