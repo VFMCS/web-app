d875e788d651a11a3fc2e66335b8c74699a4b706
lucamne <71897009+lucamne@users.noreply.github.com>
2022-11-04 15:41:52 -0400

    Bug fix

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const usrapi = require('./user-api/routes.js')
+const prdapi = require('./products-api/routes.js')
+const farmapi = require('./farmers-api/routes.js')
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/users',usrapi);
-app.use('/api/products',prdapi);
-app.use('/api/farmers',farmapi);
+app.use('/api/users', usrapi);
+app.use('/api/products', prdapi);
+app.use('/api/farmers', farmapi);
 
 
 app.listen(PORT, () => {
