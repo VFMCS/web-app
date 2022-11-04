3677d1653164ea2ad999dadff3d9c73f3d7e28b9
lucamne <negrislmn@gmail.com>
2022-11-04 09:19:22 -0400

    Featured Farmers working again

--- server/app.js
+++ server/app.js
@@ -5,12 +5,8 @@ const prdapi = require("./products-api/routes.js");
 const farmapi = require("./farmers-api/routes.js");
 const farmapi = require("./farmers-api/routes.js");
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
-
-app.use(cors());
-app.use(express.json());
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usrapi);
