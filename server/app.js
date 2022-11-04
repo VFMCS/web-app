8fbe1c454a68c5233c39507c0ee91cd3f42efc3f
lucamne <71897009+lucamne@users.noreply.github.com>
2022-11-04 02:21:21 -0400

    initial

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+<<<<<<< HEAD
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usrapi);
