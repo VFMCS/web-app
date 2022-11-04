ec436e65beb51cb9c6e9cc181dc1d36a5bbe4f31
lucamne <negrislmn@gmail.com>
2022-11-04 08:28:56 -0400

    small fix

--- server/user-api/controllers.js
+++ server/user-api/controllers.js
@@ -37,16 +37,11 @@ const deleteUser = (req,res) => {
         })
 }
 
-const getFarmers = (req,res) => {
-    pool.query(queries.getFarmers,(error,results)=>{
-        res.status(200).json(results.rows);
-    })
-}
+
 
 module.exports = {
     getUsers,
     getUserByID,
     createUser,
     deleteUser,
-    getFarmers,
 }
