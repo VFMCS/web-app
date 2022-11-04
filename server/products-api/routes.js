5ef9d5defd7d296e379106d8b1d2b495eec95fc7
lucamne <negrislmn@gmail.com>
2022-11-04 09:34:53 -0400

    Fixed featured product display

--- server/products-api/routes.js
+++ server/products-api/routes.js
@@ -2,8 +2,7 @@ const { Router } = require ('express');
 const controller = require('./controllers.js');
 
 const router = Router();
-/*
+
 router.get("/",controller.getAllProducts);
-router.get("/:product_id",controller.getProductByID);
-router.post()*/
+
 module.exports = router;
