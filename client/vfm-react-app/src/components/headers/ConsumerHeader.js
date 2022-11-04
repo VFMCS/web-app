b930e0f980f6de6dcbe9ea33dfc0b80b6676b6e6
Frankie Furnelli <fjfur10@comcast.net>
2022-11-03 20:46:40 -0400

    reorganize and changes

--- client/vfm-react-app/src/components/headers/ConsumerHeader.js
+++ client/vfm-react-app/src/components/headers/ConsumerHeader.js
@@ -4,7 +4,9 @@ import headerLogo from "../../images/logo-simple.png"
 import theme from "../../theme/theme"
 import MenuIcon from "@mui/icons-material/Menu"
 import { useNavigate } from "react-router-dom";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import SearchBar from '../SearchBar';
+import ShoppingCartButton from '../buttons/ShoppingCartButton';
+import ConsumerSidebar from '../sidebars/ConsumerSidebar';
 
 // A Header Component used by the Consumer
 // Contains: Menu button, Logo, Dashboard button, and Products Button
@@ -33,21 +35,10 @@ const ConsumerHeader = () => {
                             </Typography>
                         </Button>
                         </Box>
-                        <Box sx={{flexGrow: 2}}>
-                            <Button  variant="contained" sx={{ bgcolor: "primary.dark", fontWeight: "bold"}} startIcon={<AddCircleIcon fontSize="large" />}>
-                                Create Product
-                            </Button>
+                        <Box sx={{flexGrow: 3}}>
+                        <SearchBar />
                         </Box>
-                        <Button onClick={toDashboard}>
-                            <Typography sx={{ color: "primary.dark", fontWeight: "bold"}}>
-                                Dashboard
-                            </Typography>
-                        </Button>
-                        <Button onClick={toProducts}>
-                            <Typography sx={{ color: "primary.dark", fontWeight: "bold"}}>
-                                Your Products
-                            </Typography>
-                        </Button>
+                        <ShoppingCartButton />
                     </Toolbar>
                 </AppBar>
             </Box>
