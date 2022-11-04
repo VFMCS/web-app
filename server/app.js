1953cf288b232870c2115634c5f67251ddf079a6
lucamne <71897009+lucamne@users.noreply.github.com>
2022-11-04 02:21:21 -0400

    initial

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+<<<<<<< HEAD
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usrapi);
@@ -102,6 +103,93 @@ app.listen(PORT, () =>{
 
 })
 
+=======
+app.get('/', (req, res) => {
+    console.log(hi);
+    res.status(200).json('start');
+})
+
+
+app.get('/search/:key', cors(), (req, res) => {
+  search_query = req.params.key;
+  console.log(search_query);
+
+  const {Client} = require('pg')
+
+    const client = new Client({
+        host: "34.134.101.113",
+        user: "guest",
+        port: 5432,
+        password: "guestpass",
+        database: "vfmcs1"
+
+    })
+
+    client.connect();
+    
+    query_string = "SELECT * FROM products WHERE UPPER(CONCAT(name, '#', product_type, '#', product_category)) LIKE UPPER(" + "'%" + search_query + "%')";
+    console.log(query_string);
+
+    let data = [];
+    client.query(query_string, (err, resp) => {
+        if(!err){
+            //console.log(res.rows);
+            data = resp.rows;
+            res.send(resp.rows)
+        }
+        else{
+            console.log(err.message);
+        }
+
+        client.end;
+    })
+})
+
+app.get('/search/', cors(), (req, res) => {
+  search_query = req.params.key;
+  console.log(search_query);
+
+  const {Client} = require('pg')
+
+    const client = new Client({
+        host: "34.134.101.113",
+        user: "guest",
+        port: 5432,
+        password: "guestpass",
+        database: "vfmcs1"
+
+    })
+
+    client.connect();
+    
+    query_string = "SELECT * FROM products";
+    console.log(query_string);
+
+    let data = [];
+    client.query(query_string, (err, resp) => {
+        if(!err){
+            //console.log(res.rows);
+            data = resp.rows;
+            res.send(resp.rows)
+        }
+        else{
+            console.log(err.message);
+        }
+
+        client.end;
+    })
+})
+
+
+app.listen(PORT, () =>{
+  const url = `http://localhost:${PORT}/`
+  console.log(`Listening on Port: ${PORT}`)
+
+})
+
 /*
 const playersRouter = require("./routes/players");
-*/
+
+
+
+>>>>>>> 1afcfe3f (initial)
