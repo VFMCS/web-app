2aea4e788b56ff4079b5b3b24dffbd23f80738c3
nealshuh <nealmshah11@gmail.com>
2022-11-04 17:44:59 -0400

    Default images functionality and files
    The files for the default image were added, the text fields can only contain numbers now

--- client/vfm-react-app/src/components/buttons/UploadButton.js
+++ client/vfm-react-app/src/components/buttons/UploadButton.js
@@ -1,9 +1,12 @@
 import {Button} from "@mui/material"
 
+//An button used to designate upload images
+//takes 1 prop:
+//color: string
 
 
 const UploadButton = (props) => {
-    return (<Button onclick="imageUploader.handleUploadClick()" color={props.color || "secondary"} sx={{ p: '5', width: '100%', height: '100%'}} variant="contained" component="label">
+    return (<Button color={props.color || "secondary"} sx={{ p: '5', width: '100%', height: '100%'}} variant="contained" component="label">
                         Upload
                         <input hidden accept="image/*" multiple type="file" />
                         </Button>)
