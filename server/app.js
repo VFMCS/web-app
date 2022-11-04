7e7076c957473a4adf96d7b1683c15615bfd2842
Sai Sompally <ssompally@gmail.com>
2022-11-04 16:55:44 -0400

    listening bug

--- server/app.js
+++ server/app.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(cors());
-app.use(express.json());
+
 app.use('/api/users', usrapi);
 app.use('/api/products', prdapi);
 app.use('/api/farmers', farmapi);
