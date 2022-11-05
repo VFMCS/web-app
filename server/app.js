4404224bc9313e63283b6aa69cc0b4394e5a367b
Sai Sompally <ssompally@gmail.com>
2022-11-05 19:00:11 -0400

    fixed searching

--- server/app.js
+++ server/app.js
@@ -18,6 +18,76 @@ app.listen(PORT, () => {
   console.log(`Listening on Port: ${PORT}`)
 })
 
+app.get('/search/:key', cors(), (req, res) => {
+    search_query = req.params.key;
+    console.log(search_query);
+  
+    const {Client} = require('pg')
+  
+      const client = new Client({
+          host: "34.134.101.113",
+          user: "guest",
+          port: 5432,
+          password: "guestpass",
+          database: "vfmcs1"
+  
+      })
+  
+      client.connect();
+      
+      query_string = "SELECT * FROM products WHERE UPPER(CONCAT(name, '#', product_type, '#', product_category)) LIKE UPPER(" + "'%" + search_query + "%')";
+      console.log(query_string);
+  
+      let data = [];
+      client.query(query_string, (err, resp) => {
+          if(!err){
+              //console.log(res.rows);
+              data = resp.rows;
+              res.send(resp.rows)
+          }
+          else{
+              console.log(err.message);
+          }
+  
+          client.end;
+      })
+  })
+
+  app.get('/search/', cors(), (req, res) => {
+    search_query = req.params.key;
+    console.log(search_query);
+  
+    const {Client} = require('pg')
+  
+      const client = new Client({
+          host: "34.134.101.113",
+          user: "guest",
+          port: 5432,
+          password: "guestpass",
+          database: "vfmcs1"
+  
+      })
+  
+      client.connect();
+      
+      query_string = "SELECT * FROM products";
+      console.log(query_string);
+  
+      let data = [];
+      client.query(query_string, (err, resp) => {
+          if(!err){
+              //console.log(res.rows);
+              data = resp.rows;
+              res.send(resp.rows)
+          }
+          else{
+              console.log(err.message);
+          }
+  
+          client.end;
+      })
+  })
+
 /*
 app.use(logger("dev"));
 app.use(cors());
