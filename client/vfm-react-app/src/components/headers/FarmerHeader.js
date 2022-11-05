b46f0b16d345ffd52647349d05a3fcf3a72b276f
Frankie Furnelli <fjfur10@comcast.net>
2022-11-04 21:59:36 -0400

    change to modal

--- client/vfm-react-app/src/components/headers/FarmerHeader.js
+++ client/vfm-react-app/src/components/headers/FarmerHeader.js
@@ -1,5 +1,5 @@
 import * as React from 'react' 
-import { AppBar, Toolbar, Button, Box, ThemeProvider, CssBaseline, Typography, IconButton} from '@mui/material';
+import { AppBar, Toolbar, Button, Box, ThemeProvider, CssBaseline, Typography, IconButton, Modal, Paper} from '@mui/material';
 import headerLogo from "../../images/logo-simple.png"
 import theme from "../../theme/theme"
 import MenuIcon from "@mui/icons-material/Menu"
@@ -7,12 +7,15 @@ import { useNavigate } from "react-router-dom";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import SearchBar from '../SearchBar';
 import FarmerSidebar from '../sidebars/FarmerSidebar';
+import FarmerPostItem from '../FarmerPostItem';
 
 // A Header Component used by the Farmer
 // Contains: Menu button, Logo, Dashboard button, and Products Button
 const FarmerHeader = () => {
+    let [modalOpen, setModalState] = React.useState(false);
     let navigate = useNavigate()
     let toDashboard = () => navigate("/dashboard")
+    let toPostItem = () => setModalState(true)
     let toProducts = () => navigate("/farmer")
     let [sideBarOpen, setSidebarState] = React.useState(false)
     let toggleSidebar = () => {
@@ -39,9 +42,22 @@ const FarmerHeader = () => {
                         <Box sx={{flexGrow: 3}}>
                         <SearchBar />
                         </Box>
-                        <Button  variant="contained" sx={{ bgcolor: "primary.dark", fontWeight: "bold"}} startIcon={<AddCircleIcon fontSize="large" />}>
+                        <Button  onClick={toPostItem} variant="contained" sx={{ bgcolor: "primary.dark", fontWeight: "bold"}} startIcon={<AddCircleIcon fontSize="large" />}>
                             Create Product
                         </Button>
+                        <Modal open={modalOpen} onClose={() => setModalState(false)} closeAfterTransition sx={{display: 'flex', p: 1, alignItems: 'center', justifyContent: 'center'}}>
+                            <Box sx={{
+                                position: 'relative',
+                                bgcolor: 'background.paper',
+                                border: '2px solid #000',
+                                boxShadow: (theme) => theme.shadows[5],
+                                p: 4,
+                                }}>
+                                <Paper>
+                                    <FarmerPostItem />
+                                </Paper>
+                            </Box>
+                        </Modal>
                     </Toolbar>
                 </AppBar>
             </Box>
