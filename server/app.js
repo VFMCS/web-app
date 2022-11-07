f3d31fa541ef40ab4d373495b4511240fe9afe3e
lucamne <71897009+lucamne@users.noreply.github.com>
2022-11-06 20:19:58 -0500

    Fixed search

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const usrapi = require('./user-api/routes.js');
 const prdapi = require('./products-api/routes.js');
-const farmapi = require('./farmers-api/routes.js');
+const farmapi = require('./vendors-api/routes.js');
 const srchEndpoint = require('./search-endpoint/routes.js');
 const cors = require('cors');
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use('/api/users', usrapi);
 app.use('/api/products', prdapi);
-app.use('/api/farmers', farmapi);
+app.use('/api/vendors', farmapi);
 app.use('/search', srchEndpoint);
 
 
