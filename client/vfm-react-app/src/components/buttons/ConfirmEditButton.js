109f75a1a7d39ccdc7c941d857ef8b970eaff7a5
mvigneras <maxence.vigneras@gmail.com>
2022-11-06 22:27:06 -0500

    Connect font and backend farmer create product

--- client/vfm-react-app/src/components/buttons/ConfirmEditButton.js
+++ client/vfm-react-app/src/components/buttons/ConfirmEditButton.js
@@ -10,7 +10,6 @@ import {Button} from "@mui/material";
 //label: string (will be converted to all caps)
 
 const ConfirmEditButton = (props) => {
-
     return (
         <Button variant={props.variant || "text"} style={{ height: '100%', width: '50%'}} size={props.size || "medium"} color={props.color || "success"} sx={{fontWeight: "bold"}}>{props.label || "Publish"}</Button>
     )
