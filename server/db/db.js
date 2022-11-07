698a30b8b5aa3ab0369c34212bcb1afadba6ded6
Sai Sompally <ssompally@gmail.com>
2022-11-07 13:22:36 -0500

    modified database connections to work for each OS

--- server/db/db.js
+++ server/db/db.js
@@ -13,13 +13,30 @@ const pool = new Pool({
 
 //This is for local development, will potentially need modification for deployment
 const startAuthProxy = () => {
+    
     instance = `radiant-saga-366418:us-central1:vfmcs-db=tcp:${port}`;
     cred_file = "./db/db-service-account.json";
     console.log(`Starting cloud sql auth proxy on port: ${port}...`);
-    exec('./db/cloud_sql_proxy.exe', ["-instances=" + instance, "-credential_file=" + cred_file], (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    })
+    
+    if(process.platform === 'darwin'){
+        exec('./db/cloud_sql_proxy_mac', ["-instances=" + instance, "-credential_file=" + cred_file], (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        })
+    }
+    else if(process.platform === 'win32'){
+        exec('./db/cloud_sql_proxy.exe', ["-instances=" + instance, "-credential_file=" + cred_file], (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        })
+    }
+    else if(process.platform === 'linux'){
+        exec('./db/cloud_sql_proxy_linux', ["-instances=" + instance, "-credential_file=" + cred_file], (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        })
+
+    }
 }
 
 const pool = new Pool({
