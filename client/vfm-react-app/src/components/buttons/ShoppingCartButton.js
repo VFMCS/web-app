f20e4822134d8dfcfe1c1af3e1b2ba0057d0677d
AJ Yeung <ajyeung123@gmail.com>
2022-11-08 00:18:04 -0500

    upload and shoppingcart button tests

--- client/vfm-react-app/src/components/buttons/ShoppingCartButton.js
+++ client/vfm-react-app/src/components/buttons/ShoppingCartButton.js
@@ -9,7 +9,7 @@ import {Button} from "@mui/material"
 // TODO: Add action to link to sign up page
 const ShoppingCartButton = (props) => {
     return (
-        <Button color={props.color || "secondary"} variant="contained" sx={{fontWeight: "bold"}}> 
+        <Button data-testid = "shopping" color={props.color || "secondary"} variant="contained" sx={{fontWeight: "bold"}}> 
             <ShoppingCartCheckoutIcon />
         </Button>
     )
