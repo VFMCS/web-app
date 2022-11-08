1513f27f3db2b23945ff8e5b49a760b0020f2d2c
lucamne <71897009+lucamne@users.noreply.github.com>
2022-11-08 02:34:14 -0500

    fixed create user bug

--- server/user-api/queries.js
+++ server/user-api/queries.js
@@ -1,6 +1,6 @@
 const getUsers = "SELECT * FROM USERS";
 const getUserByID = "SELECT * FROM USERS WHERE user_id = $1";
-const createUser = "INSERT INTO users (username,password,first_name,last_name,address,is_vendor,about_me,email) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)";
+const createUser = "INSERT INTO users (password,first_name,last_name,address,is_vendor,about_me,email) VALUES ($1,$2,$3,$4,$5,$6,$7)";
 const deleteUser = "DELETE FROM users WHERE user_id = $1";
 
 const getFarmers = "SELECT * FROM users WHERE is_vendor = true";
