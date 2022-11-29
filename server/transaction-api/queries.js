e56659394ac5fe8b719590a0bbb311ab44dc762c
lucamne <71897009+lucamne@users.noreply.github.com>
2022-11-29 01:16:25 -0500

    update queries

--- server/transaction-api/queries.js
+++ server/transaction-api/queries.js
@@ -2,9 +2,11 @@ const addTransaction = "INSERT INTO transactions (vendor_id,customer_id,quantity
 const updateQuantity = "UPDATE transactions SET quantity = $1 WHERE transaction_id = $2";
 
 const getCustomerCart = "SELECT * FROM transactions WHERE customer_id = $1";
+const getVendorOrders = "SELECT * FROM transactions WHERE vendor_id = $1 AND in_cart = TRUE";
 
 module.exports = {
     addTransaction,
     updateQuantity,
     getCustomerCart,
+    getVendorOrders,
 }
