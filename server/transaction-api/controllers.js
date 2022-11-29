f0fb4dcd76e3f86b640cba62c79a8c10ccc6831d
lucamne <71897009+lucamne@users.noreply.github.com>
2022-11-29 00:52:46 -0500

    controller update

--- server/transaction-api/controllers.js
+++ server/transaction-api/controllers.js
@@ -18,10 +18,23 @@ const addTransaction = (req, res) => {
     })
 }
 
+//send null for values not being updated
 const updateTransaction = (req, res) => {
-    const { transaction_id, quantity, isInCart, isReserved } = req.body;
-    if (isInCart != null) {
+    const { transaction_id, quantity, is_InCart, is_Reserved } = req.body;
+    if (quantity != null) {
         pool.query(queries.updateQuantity, [quantity, transaction_id], (error, results) => {
+            if (error) throw error;
+            res.status(200).json(results.rows);
+        })
+    }
+    if (is_InCart != null) {
+        pool.query(queries.updateCartStatus, [is_InCart, transaction_id], (error, results) => {
+            if (error) throw error;
+            res.status(200).json(results.rows);
+        })
+    }
+    if (is_Reserved != null) {
+        pool.query(queries.updateReserveStatus, [is_Reserved, transaction_id], (error, results) => {
             if (error) throw error;
             res.status(200).json(results.rows);
         })
