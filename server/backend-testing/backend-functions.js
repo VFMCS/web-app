67728515db7155f12a0c391dbda465ba66c18070
Sai Sompally <ssompally@gmail.com>
2022-12-08 22:14:54 -0500

    redo backend tests

--- server/backend-testing/backend-functions.js
+++ server/backend-testing/backend-functions.js
@@ -1,16 +1,4 @@
 const mathOperations = {
-    sum: function(a,b) {
-    return a + b;
-    },
     
-    diff: function(a,b) {
-    return a - b;
-    },
-    product: function(a,b) {
-    return a * b;
-    },
-    divide: function(a,b) {
-    return a / b;
-    }
     }
 module.exports = mathOperations
