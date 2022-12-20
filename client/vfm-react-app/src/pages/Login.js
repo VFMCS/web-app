1d855086babc293a6691042d9c98d072ce2571d9
lucamne <71897009+lucamne@users.noreply.github.com>
2022-12-20 16:48:06 -0500

    change to https

--- client/vfm-react-app/src/pages/Login.js
+++ client/vfm-react-app/src/pages/Login.js
@@ -4,7 +4,7 @@ import "../login-styles/auth.css";
 import LoginHeader from '../components/headers/LandingHeader.js';
 import LandingHeader from '../components/headers/LandingHeader.js';
 import { useNavigate } from 'react-router-dom';
-import {Stack} from '@mui/material'
+import { Stack } from '@mui/material'
 
 
 // Provides an interface, when Log-in button is clicked, for users to log in. 
@@ -43,9 +43,9 @@ const Login = () => {
     if (login.email === "" || login.password === "") {
       console.log("error"); return;
     }
-    
+
     //Get the user by email and id
-    let url = 'http://localhost:5001/api/users/' + login.email + "/" + login.password;
+    let url = 'https://localhost:5001/api/users/' + login.email + "/" + login.password;
     console.log(url);
     fetch(url).then(response => response.json()).then(data => {
       if (data.length === 0) {
@@ -56,64 +56,64 @@ const Login = () => {
       localStorage.setItem('curr_user_id', data[0].user_id)
     }).then(() => {
       console.log("curr_user_id: " + localStorage.getItem('curr_user_id'));
-      fetch('http://localhost:5001/curr-user-api/' + localStorage.getItem('curr_user_id')).then(response => response.json()).then(data => {
+      fetch('https://localhost:5001/curr-user-api/' + localStorage.getItem('curr_user_id')).then(response => response.json()).then(data => {
         console.log(data);
-        localStorage.setItem('curr_user_is_vendor', data[0].is_vendor) 
-       
+        localStorage.setItem('curr_user_is_vendor', data[0].is_vendor)
+
       }).then(() => {
         console.log("curr_user_is_vendor: " + localStorage.getItem('curr_user_is_vendor'));
-        if(localStorage.getItem('curr_user_is_vendor') === 'true') {
+        if (localStorage.getItem('curr_user_is_vendor') === 'true') {
           navigate('/farmer');
         }
         else {
           navigate('/customer');
         }
       }
-    ).catch(err => console.error(err));     
-  }).catch(err => console.error(err));
-}
+      ).catch(err => console.error(err));
+    }).catch(err => console.error(err));
+  }
   return (
     <Stack spacing={4} minHeight={"92vh"}>
-    <div id="login-auth-wrap" className="auth-wrapper">
-        <LandingHeader/>
+      <div id="login-auth-wrap" className="auth-wrapper">
+        <LandingHeader />
 
-      <div className="auth-inner">
-        <form onSubmit={handleSubmit}>
-          <h3>Sign In</h3>
+        <div className="auth-inner">
+          <form onSubmit={handleSubmit}>
+            <h3>Sign In</h3>
 
-          <div className="form-group">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-              name="email"
-              value={login.email}
-              onChange={handleChange}
-            />
-          </div>
+            <div className="form-group">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                name="email"
+                value={login.email}
+                onChange={handleChange}
+              />
+            </div>
 
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter password"
-              name="password"
-              value={login.password}
-              onChange={handleChange}
-            />
-          </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                name="password"
+                value={login.password}
+                onChange={handleChange}
+              />
+            </div>
 
-          <button type="submit" className="btn btn-primary btn-block">
-            Submit
-          </button>
-          <p className="forgot-password text-right">
-            Forgot <a href="#">password?</a>
-          </p>
-        </form>
+            <button type="submit" className="btn btn-primary btn-block">
+              Submit
+            </button>
+            <p className="forgot-password text-right">
+              Forgot <a href="#">password?</a>
+            </p>
+          </form>
+        </div>
       </div>
-    </div>
     </Stack>
   );
 };
--- client/vfm-react-app/src/pages/Register.js
+++ client/vfm-react-app/src/pages/Register.js
@@ -5,7 +5,7 @@ import LoginHeader from '../components/headers/LandingHeader.js';
 import FarmerLandingPage from "./FarmerLandingPage.js";
 import { NavigateBefore } from '@mui/icons-material';
 import FarmerProfileModal from "../components/FarmerProfileModal.js"
-import {Alert, Stack} from "@mui/material"
+import { Alert, Stack } from "@mui/material"
 
 // When any sign-up button is clicked, this modal provides a place for vendors or farmers to create a new account. 
 // It makes sure no fields are left blank and all of the information entered is valid for our database by providing the user
@@ -21,14 +21,14 @@ export const Register = () => {
     password: "",
     first_name: "",
     last_name: "",
-    address:"",
-    is_vendor:false,
-    photo:null,
-    about_me:"",
+    address: "",
+    is_vendor: false,
+    photo: null,
+    about_me: "",
     email: "",
     created_on: new Date(),
-    image_url:"",
-    role:"farmer"
+    image_url: "",
+    role: "farmer"
   });
 
   let [missingFieldError, setMissingFieldError] = useState(false);
@@ -51,31 +51,32 @@ export const Register = () => {
   };
 
   React.useEffect(() => {
-   setCredentials({
-    password: credentials.password,
-    first_name: credentials.first_name,
-    last_name: credentials.last_name,
-    address:"",
-    is_vendor: credentials.is_vendor === "farmer",
-    photo:null,
-    about_me:"",
-    email: credentials.email,
-    created_on: new Date(),
-    image_url:"",
-   })
-}, [])
+    setCredentials({
+      password: credentials.password,
+      first_name: credentials.first_name,
+      last_name: credentials.last_name,
+      address: "",
+      is_vendor: credentials.is_vendor === "farmer",
+      photo: null,
+      about_me: "",
+      email: credentials.email,
+      created_on: new Date(),
+      image_url: "",
+    })
+  }, [])
 
   const postCredentials = () => { // Saves the credentials of a new user to the database. Prints all to console first.
     console.log('post starting');
 
-    fetch("http://localhost:5001/api/users", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(credentials)}).then(data => {console.log(data);
+    fetch("https://localhost:5001/api/users", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(credentials) }).then(data => {
+      console.log(data);
       //Get the user by email and password to then retrieve the id
-      let url = 'http://localhost:5001/api/users/' + credentials.email + "/" + credentials.password;
+      let url = 'https://localhost:5001/api/users/' + credentials.email + "/" + credentials.password;
       console.log(url);
       fetch(url).then(response => response.json()).then(data => {
         console.log("data: " + data)
-        console.log("login user id: " + data[0].user_id); 
-        setCurr_User_Id(data[0].user_id); 
+        console.log("login user id: " + data[0].user_id);
+        setCurr_User_Id(data[0].user_id);
         localStorage.setItem('curr_user_id', data[0].user_id);
         localStorage.setItem('curr_user_is_vendor', credentials.is_vendor);
       }).then(() => {
@@ -84,139 +85,139 @@ export const Register = () => {
         console.log(credentials);
       }).then(() => {
         //set curr_user_id and print values
-        if(localStorage.getItem('curr_user_is_vendor') === 'true'){navigate('/farmer');}
-        else{navigate('/customer');}
+        if (localStorage.getItem('curr_user_is_vendor') === 'true') { navigate('/farmer'); }
+        else { navigate('/customer'); }
       });
     });
   }
-    
-  
+
+
   const handleSubmit = (e) => { // On submit of the first registering button. After this is submitted, the modal opens.
     e.preventDefault();
 
     credentials.is_vendor = credentials.is_vendor === "farmer";
     console.log(credentials.is_vendor)
 
-    let required = [credentials.first_name, 
-      credentials.last_name, 
-      credentials.password, 
-      credentials.email]
+    let required = [credentials.first_name,
+    credentials.last_name,
+    credentials.password,
+    credentials.email]
     if (required.includes("")) {
       console.log(required)
       setMissingFieldError(true)
       return
     }
-    if (is_vendor_options.map(o => document.getElementById(o.value).checked).every(x=>!x)) {
+    if (is_vendor_options.map(o => document.getElementById(o.value).checked).every(x => !x)) {
       setMissingFieldError(true)
       return
     }
 
     setMissingFieldError(false)
-  
-    if(credentials.is_vendor){
+
+    if (credentials.is_vendor) {
       setProfileModalState(true)
     }
-    else{
+    else {
       postCredentials()
-      
+
     }
   };
 
-  
+
   let modalCloseHandler = (e, reason) => { // Modal closes finally once pictures are uploaded.
-      if (reason === "backdropClick") {
-          return
-      }
-      setProfileModalState(false)
-      postCredentials()
-      localStorage.setItem('curr_user_is_vendor', true)
-      navigate('/farmer')
+    if (reason === "backdropClick") {
+      return
+    }
+    setProfileModalState(false)
+    postCredentials()
+    localStorage.setItem('curr_user_is_vendor', true)
+    navigate('/farmer')
   }
 
   return (
-      <Stack spacing={4} minHeight={"92vh"}>
+    <Stack spacing={4} minHeight={"92vh"}>
       <div id="auth-wrapper-register" className="auth-wrapper">
-      <LoginHeader />
-      <div className="auth-inner">
-        <form onSubmit={handleSubmit}>
-          <h3>Sign Up</h3>
+        <LoginHeader />
+        <div className="auth-inner">
+          <form onSubmit={handleSubmit}>
+            <h3>Sign Up</h3>
 
-          <div className="form-group">
-            <label>First name</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="First name"
-              name="first_name"
-              value={credentials.first_name}
-              onChange={handleChange}
-            />
-          </div>
+            <div className="form-group">
+              <label>First name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                name="first_name"
+                value={credentials.first_name}
+                onChange={handleChange}
+              />
+            </div>
 
-          <div className="form-group">
-            <label>Last name</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Last name"
-              name="last_name"
-              value={credentials.last_name}
-              onChange={handleChange}
-            />
-          </div>
+            <div className="form-group">
+              <label>Last name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+                name="last_name"
+                value={credentials.last_name}
+                onChange={handleChange}
+              />
+            </div>
 
-          <div className="form-group">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-              name="email"
-              value={credentials.email}
-              onChange={handleChange}
-            />
-          </div>
+            <div className="form-group">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                name="email"
+                value={credentials.email}
+                onChange={handleChange}
+              />
+            </div>
 
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter password"
-              name="password"
-              value={credentials.password}
-              onChange={handleChange}
-            />
-          </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                name="password"
+                value={credentials.password}
+                onChange={handleChange}
+              />
+            </div>
 
-          <div className="radio-group">
-            <label>Role</label>
-            {is_vendor_options.map((is_vendor) => (
-              <div className="radio-item" key={is_vendor.value}>
-                <input
-                  type="radio"
-                  id={is_vendor.value}
-                  name="is_vendor"
-                  value={is_vendor.value}
-                  onChange={handleChange}
-                />
+            <div className="radio-group">
+              <label>Role</label>
+              {is_vendor_options.map((is_vendor) => (
+                <div className="radio-item" key={is_vendor.value}>
+                  <input
+                    type="radio"
+                    id={is_vendor.value}
+                    name="is_vendor"
+                    value={is_vendor.value}
+                    onChange={handleChange}
+                  />
 
-                <label htmlFor={is_vendor.value}>{is_vendor.label}</label>
-              </div>
-            ))}
-          </div>
-          { missingFieldError &&
-          <Alert severity="error" sx={{m: 2}}> Please enter all fields </Alert>}
-          <button type="submit" className="btn btn-primary btn-block">
-            Sign Up
-          </button>
-          <p className="forgot-password text-right">
-            Already registered <Link to="/sign-in">sign in?</Link>
-          </p>
-        </form>
+                  <label htmlFor={is_vendor.value}>{is_vendor.label}</label>
+                </div>
+              ))}
+            </div>
+            {missingFieldError &&
+              <Alert severity="error" sx={{ m: 2 }}> Please enter all fields </Alert>}
+            <button type="submit" className="btn btn-primary btn-block">
+              Sign Up
+            </button>
+            <p className="forgot-password text-right">
+              Already registered <Link to="/sign-in">sign in?</Link>
+            </p>
+          </form>
+        </div>
+        <FarmerProfileModal open={profileModalOpen} setModalState={setProfileModalState} onClose={modalCloseHandler} profile={credentials} changeHandler={handleChange} />
       </div>
-      <FarmerProfileModal open={profileModalOpen} setModalState={setProfileModalState} onClose={modalCloseHandler} profile={credentials} changeHandler={handleChange}/>
-    </div>
     </Stack>
   );
 };
